ebb161699187d4e617de300901e5dc0da51124ea
Daniel Persson <mailto.woden@gmail.com>
2018-05-25 12:59:41 +0200

    Added more lighthouse rules.

--- lighthouse_test.sh
+++ lighthouse_test.sh
@@ -15,6 +15,5 @@ if [ "${TRAVIS_TEST_RESULT}" = "1" ]; then
   exit
 fi
 
-firebase use --token $FIREBASE_TOKEN staging
-firebase deploy -m "Pipeline $CI_PIPELINE_ID, build $CI_BUILD_ID" --non-interactive --token $FIREBASE_TOKEN
+firebase deploy -m "Pipeline $CI_PIPELINE_ID, build $CI_BUILD_ID" --non-interactive --token $FIREBASE_TOKEN --project repnet-staging
 npm run lh $FIREBASE_HOST
