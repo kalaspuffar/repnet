9f9250f08d163c5eb812d35fccc09d291849be3a
Daniel Persson <mailto.woden@gmail.com>
2018-05-26 11:23:29 +0200

    New design.

--- src/repnet-app.js
+++ src/repnet-app.js
@@ -3,19 +3,61 @@ import {LitElement, html} from '../node_modules/@polymer/lit-element/lit-element
 export class RepnetApp extends LitElement {
   static get properties() {
     return {
-      name: {
-        Type: String,
-      },
     };
   }
 
   constructor() {
     super();
-    this.name = 'Explorers';
   }
 
-  _render({name}) {
-    return html`<h2>Welcome!</h2>`;
+  _render() {
+    return html`
+      <style>
+        * {
+          margin: 0;
+          padding: 0;
+        }
+        
+        .mid-banner {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 300px;
+          width: 100vw;
+          margin: auto 0;
+          background-color: #FCE4EC;
+          box-shadow: inset 0px 5px 10px -3px rgba(0,0,0,0.5);
+        }
+
+        .fullscreen {
+          display: flex;
+          width: 100vw;
+          height: 100vh;
+          background-color: black;
+        }      
+
+        span {
+          display: block;
+          margin: 0 auto;          
+        }
+        h2 {
+          margin: 0 auto;          
+        }
+
+        h1, h2, h3 {
+          padding: 5px;
+        }
+      </style>
+      <div class="fullscreen">
+        <div class="mid-banner">
+          <img width="100px" alt="Spider web" src="../images/spider-web.png" />          
+          <h1>RepNet</h1>
+          <h2>The network where reputation matters</h2>
+          <h3>We bind trolls and promote innovation</h3>
+        </div>
+      </div>
+    `;
   }
 }
 
